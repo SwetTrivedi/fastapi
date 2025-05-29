29dc0fb34343f4238d47ac7df23917d3f0f8f9ed
SwetTrivedi <swettrivedi981@gmail.com>
2025-05-29 18:36:17 +0530

    sql

--- formhandling/app/main.py
+++ formhandling/app/main.py
@@ -10,27 +10,27 @@ app=FastAPI()
 
 # Simple HTML form for testing
 # application/x-www-form-urlencoded 
-# @app.get("/",response_class=HTMLResponse)
-# async def get_form():
-#     return """ 
-#     <html>
-#         <body>
-#             <h2>Login Form</h2>
-#             <Form action="/login/" method="post">
-#                 <label type="username">Username:<label><br>
-#                 <input type="text" id="username" name="username"><br>
-#                 <label type="password">Password:<label><br>
-#                 <input type="password" id="password" name="password"><br>
-#                 <input type="submit" value="submit">
-#             </form>
-#         </body>
-#     </html>
-#     """
+@app.get("/",response_class=HTMLResponse)
+async def get_form():
+    return """ 
+    <html>
+        <body>
+            <h2>Login Form</h2>
+            <Form action="/login/" method="post">
+                <label type="username">Username:<label><br>
+                <input type="text" id="username" name="username"><br>
+                <label type="password">Password:<label><br>
+                <input type="password" id="password" name="password"><br>
+                <input type="submit" value="submit">
+            </form>
+        </body>
+    </html>
+    """
 
 
-# @app.post("/login/")
-# async def login(username:Annotated[str,Form()],password:Annotated[str,Form()]):
-#     return{"username":username,"password_length":len(password)}
+@app.post("/login/")
+async def login(username:Annotated[str,Form()],password:Annotated[str,Form()]):
+    return{"username":username,"password_length":len(password)}
 
 
 # form with validation 
